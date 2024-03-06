2c92feb100c75d224e41f5c881279c44f60d3695
danielcarlesso <daniel.carlesso@studenti.unipd.it>
2024-03-06 15:19:46 +0100

    NearestNeighbor ready to test

--- tsp/algorithms/nearestneighbor/nearestneighbor.c
+++ tsp/algorithms/nearestneighbor/nearestneighbor.c
@@ -6,29 +6,34 @@
 */
 
 #include <stdio.h> 
+#include <stdlib.h>
 #include <time.h>
+#include <assert.h>
 
 #include "nearestneighbor.h"
 #include "../../utility/utility.h"
 
 /*
-* Returns index of next node to visit (nearest neighbor).
-*
+* Finds index of next node to visit (nearest neighbor), puts next node to visit close to last visited and updates cost of the solution.
+* IP len current index we have to update in $sol->succ
+* IP inst tsp instance
+* IOP sol solution we want to update
 */
-static int minDistNode(int len, const TSPSolution* sol, const TSPInstance* inst){
-    int i, curr;
+static void visitNext(int len, const TSPInstance* inst, TSPSolution* sol){
+    int i;
 
-    int last = sol->succ[len-1] - 1;
+    int last = sol->succ[len-1];
+    int curr = sol->succ[len];
     int next = len;
 
-    double min_dist = inst->dist[last][next];
+    double min_dist = getDist(last, curr);
     double curr_dist;
     
     for(i=len+1; i<inst->dimension; i++){
         
-        curr = sol->succ[i] - 1; /* index in inst->distances of next unvisited node */
+        curr = sol->succ[i]; /* index in inst->distances of next unvisited node */
 
-        curr_dist = inst->dist[last][curr]; /* get distance from last visited node and the current unvisited node in succ */
+        curr_dist = getDist(last, curr); /* get distance from last visited node and the current unvisited node in succ */
 
         if(curr_dist < min_dist){
             min_dist = curr_dist;
@@ -36,7 +41,8 @@ static int minDistNode(int len, const TSPSolution* sol, const TSPInstance* inst)
         }
     }
 
-    return next;
+    swapInt(&(sol->succ[len]), &(sol->succ[next]));
+    sol->val += min_dist;
 
 }/* minDistNode */
 
@@ -53,7 +59,7 @@ void NN_initSol(const TSPInstance* inst, TSPSolution* sol){
         sol->succ[i] = i;
     
     while(true){
-        i = read_int("Insert starting node: ");
+        i = readInt("Insert starting node: ");
 
         if(i < 0 || i >= inst->dimension)
             printf("Invalid starting node.\n");
@@ -61,7 +67,7 @@ void NN_initSol(const TSPInstance* inst, TSPSolution* sol){
             break;
     }
     
-    swap_int(&(sol->succ[0]), &(sol->succ[i-1])); /* i-th node in position i-1 */
+    swapInt(&(sol->succ[0]), &(sol->succ[i-1])); /* i-th node in position i-1 */
 
 }/* initSol */
 
@@ -72,27 +78,74 @@ void NN_initSol(const TSPInstance* inst, TSPSolution* sol){
 */
 int NN_solver(const TSPInstance* inst, TSPSolution* sol){
     int len=1;
-    int next;
-    time_t curr_time;
+    clock_t start = clock();
     
     while(len < inst->dimension){
 
-        next = minDistNode(len, sol, inst);
-
-        swap_int(&(sol->succ[len]), &(sol->succ[next]));
+        visitNext(len, inst, sol);
 
         len++;
     }
 
-    time(&curr_time); /* store current time in curr_time */
+    sol->val += getDist(sol->succ[0], sol->succ[len-1]); /* add cost of connection of last to first node */
 
-    return (int)curr_time;
+     /* store current time in curr_time */
+
+    return getSeconds(start, clock());
 
 }/* NN_solver */
 
 /*
-* 
+* Checks whether the solution is valid.
+* IP inst tsp instance
+* IP sol solution we want to check validity of
+* OP false if there is no error, true otherwise.
 */
-void NN_controller(void){
+bool NN_controller(const TSPInstance* inst, const TSPSolution* sol){
+    int i;
+    bool valid;
+
+    double cost = 0.0;
+    int* counters = malloc(inst->dimension * sizeof(int));
+
+    assert(counters != NULL);
+
+    for(i=1; i<inst->dimension; i++){
+        
+        ++counters[sol->succ[i] - 1];
+
+        cost += inst->dist[sol->succ[i-1] - 1][sol->succ[i] - 1];
+    }
+    
+    cost += inst->dist[sol->succ[i-1] - 1][sol->succ[0] - 1]; /* add cost of connection of last to first node */
+
+    valid = isEqualPrecision(sol->val, cost, EPSILON);
+
+    for(i=0; i<inst->dimension; i++){
+        if(counters[i] != 1)
+            valid = false;
+    }
+
+    free(counters);
+
+    return !valid;
 
 }/* NN_controller */
+
+/*
+* IP set settings
+* IP inst tsp instance
+* IOP sol solution
+* OP false if found a valid solution, true otherwise.
+*/
+bool nearestNeighbor(const Settings* set, const TSPInstance* inst, TSPSolution* sol){
+    int time;
+    bool error;
+
+    NN_initSol(inst, sol);
+
+    time = NN_solver(inst, sol);
+
+    return NN_controller(inst, sol);
+
+}/* nearestNeighbor */
--- tsp/tsp.c
+++ tsp/tsp.c
@@ -10,6 +10,7 @@
 #include <assert.h>
 
 #include "tsp.h"
+#include "algorithms/nearestneighbor/nearestneighbor.h"
 
 /*
 * IP n number of nodes of the instance
@@ -54,7 +55,6 @@ void freeSol(TSPSolution* sol){
 * IP inst instance to print
 */
 void printInst(const TSPInstance* inst){
-    
 	int i;
 
     printf("Instance:\n");
@@ -100,3 +100,41 @@ void plotSolution(const TSPSolution* sol, const TSPInstance* inst){
     pclose(gnuplotPipe);
 
 }/* plotSolution */
+
+/*
+* Print algorithm legend.
+*/
+void algorithmLegend(void){
+    
+    printf("Available algorithms:\n");
+    printf("Code: %d, Algorithm: Just a random solution\n", RANDOM);
+    printf("Code: %d, Algorithm: Nearest neighbor search\n", NEAREST_NEIGHBOR);
+    printf("\n");
+    
+}/* algorithmLegend */
+
+/*
+* IP alg algorithm to run
+* IP inst tsp instance
+* IOP sol solution 
+* IP set settings
+* OP error true if an error occurred, false otherwise.
+*/
+bool run(int alg, const TSPInstance* inst, TSPSolution* sol, const Settings* set){
+    bool error = false;
+    
+    switch (alg){
+    case RANDOM:
+        /* code */
+        break;
+    case NEAREST_NEIGHBOR:
+        error = nearestNeighbor(set, inst, sol);
+        break;
+    default:
+        printf("Error: Algorithm code not found.\n\n");
+        return true;
+    }
+
+    return error;
+
+}/* run */
