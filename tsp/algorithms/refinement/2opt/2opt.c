0f5ec55824be9a85d2b8e303ad2b23d69d38a4d4
Luigi Frigione <lfrigione@gmail.com>
2024-03-10 13:22:34 +0100

    delta cost

--- tsp/algorithms/refinement/2opt/2opt.c
+++ tsp/algorithms/refinement/2opt/2opt.c
@@ -10,6 +10,36 @@
 #include "../../../utility/utility.h"
 
 /*
+* IP i sol->succ first node index of the move
+* IP j sol->succ second node index of the move
+* IP inst tsp instance
+* IP sol refined solution
+* OR the cost of the move
+*/
+double delta(int i, int j, const TSPInstance* inst, const TSPSolution* sol){
+
+	int a = (*sol).succ[i], a1 = (*sol).succ[(i + 1) % (*inst).dimension], b = (*sol).succ[j], b1 = (*sol).succ[(j + 1) % (*inst).dimension];
+
+	return getDist(a, b, inst) + getDist(a1, b1, inst) - (getDist(a, a1, inst) + getDist(b, b1, inst));
+
+}/* delta */
+
+/*
+* IP inst tsp instance
+* IOP sol refined solution
+* NB: we have to avoid two cases, the ones when one of the two selected nodes is the successor of the other. In this case when we
+* 		break the cycle we are no longer able to reconstruct it.
+*/
+void opt2(const TSPInstance* inst, TSPSolution* sol){
+
+	int i;
+	int j;
+	double delta_cost;
+
+
+}/* opt2 */
+
+/*
 * IP alg refinement algorithm to run
 * IP inst tsp instance
 * IOP sol refined solution
