398ddb754eba15bed8d5a650c06f2a8cc05e1f8e
Luigi Frigione <lfrigione@gmail.com>
2024-05-21 09:24:50 +0200

    perfprof zoom script

---
+++ perfprof/data/zoom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Example: ./zoom.sh 1.025 seed_default_tl_120s_nodes_1000_random
+# 1.025 -> max asix value
+# seed_default_tl_120s_nodes_1000_random -> zoom target
+
+python3 ../perfprof.py -D , -M $1 -T 120 ./$2/$2.csv ./$2/$2\_zoom.pdf -X "Cost Ratio" -P "TSP Performance Profile"
