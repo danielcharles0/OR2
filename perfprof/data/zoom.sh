41bb839994a10f7475525d6ce304c1ef15dd2567
Luigi Frigione <lfrigione@gmail.com>
2024-07-05 21:32:12 +0200

    hard fixing perfprof

--- perfprof/data/zoom.sh
+++ perfprof/data/zoom.sh
@@ -4,4 +4,4 @@
 # 1.025 -> max asix value
 # seed_default_tl_120s_nodes_1000_random -> zoom target
 
-python3 ../perfprof.py -D , -M $1 -T 120 ./heuristics/$2/$2.csv ./heuristics/$2/$2\_zoom.pdf -X "Cost Ratio" -P "TSP Performance Profile"
+python3 ../perfprof.py -D , -M $1 -T 120 ./matheuristics/$2/$2.csv ./matheuristics/$2/$2\_zoom.pdf -X "Cost Ratio" -P "TSP Performance Profile"
